a1a131c6791ec9d01b43ffe59c7ace9f229c5110
jactpc <josecortez2525.70@gmail.com>
2025-02-05 19:41:40 -0400

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Videollamada con WebRTC</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <h1>Videollamada WebRTC</h1>
+    <video id="localVideo" autoplay muted></video>
+    <video id="remoteVideo" autoplay></video>
+    <script src="js/webrtc.js"></script>
+</body>
+</html>
